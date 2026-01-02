c2b525aa2434dc120cc365416c80a8e70dd59ade
rafaef35rt <rafalef35r@gmail.com>
2026-01-02 19:27:37 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "timetable-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
